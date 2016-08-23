91b78ddbdfdae79269828ee3b7037f1da59f1e49
PatrickJS <github@gdi2290.com>
2016-08-22 23:01:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ function Angular2HMRLoader(source, sourcemap) {
   source = source.replace(bootstrapModule, function (match, boot, ngmodule, offset, src) {
     // return updated metadata
     return boot + '(' + ngmodule + ')' +
-    '.then(function(MODULE_REF: any) {' +
-      'if (module.hot) {\n'+
-        'module.hot.accept();\n'+
+    '.then(function(MODULE_REF) {' +
+      'if (module["hot"]) {\n'+
+        'module["hot"]["accept"]();\n'+
         '\n'+
         'if (MODULE_REF.instance.hmrOnInit) {\n'+
-          'MODULE_REF.instance.hmrOnInit(module.hot.data);\n'+
+          'MODULE_REF.instance.hmrOnInit(module["hot"]["data"]);\n'+
         '}\n'+
         'if (MODULE_REF.instance.hmrOnStatus) {\n'+
-          'module.hot.apply(function(status) {\n'+
+          'module["hot"]["apply"](function(status) {\n'+
             'MODULE_REF.instance.hmrOnStatus(status);\n'+
           '});\n'+
         '}\n'+
         'if (MODULE_REF.instance.hmrOnCheck) {\n'+
-          'module.hot.check(function(err, outdatedModules) {\n'+
+          'module["hot"]["check"](function(err, outdatedModules) {\n'+
             'MODULE_REF.instance.hmrOnCheck(err, outdatedModules);\n'+
           '});\n'+
         '}\n'+
         'if (MODULE_REF.instance.hmrOnDecline) {\n'+
-          'module.hot.decline(function(dependencies) {\n'+
+          'module["hot"]["decline"](function(dependencies) {\n'+
             'MODULE_REF.instance.hmrOnDecline(dependencies);\n'+
           '});\n'+
         '}\n'+
-        'module.hot.dispose(function(store) {\n'+
+        'module["hot"]["dispose"](function(store) {\n'+
           'MODULE_REF.instance.hmrOnDestroy && MODULE_REF.instance.hmrOnDestroy(store);\n'+
           'MODULE_REF.destroy();\n'+
           'MODULE_REF.instance.hmrAfterDestroy && MODULE_REF.instance.hmrAfterDestroy(store);\n'+
